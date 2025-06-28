a8227733ed40f1b208ab1ef04ac02f28f7744d34
Marius Wespe <mariuswespe@gmail.com>
2025-06-28 18:18:04 +0200

    Added cube spawner, not working so far

---
+++ CubeSpawner.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class CubeSpawner : Node3D
+{
+	[Export] PackedScene CubeScene { get; set; }
+
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_accept"))
+		{
+			GD.Print("Spawning Cube");
+
+			if (CubeScene == null)
+			{
+				GD.Print("CubeScene is not set!");
+				return;
+			}
+
+			Node3D newCube = (Node3D)CubeScene.Instantiate();
+
+			AddChild(newCube);
+		}
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
