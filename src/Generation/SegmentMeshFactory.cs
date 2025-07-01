4332215b909949640f582c5f84df2199e4fec1b8
Marius Wespe <mariuswespe@gmail.com>
2025-07-01 18:54:13 +0200

    feat: Created placeholder SegmentMeshFactory

---
+++ src/Generation/SegmentMeshFactory.cs
@@ -0,0 +1,12 @@
+using PetriDish.Genome;
+using Godot;
+namespace PetriDish.Generation;
+
+public static class SegmentMeshFactory
+{
+    public static ArrayMesh CreateMesh(SegmentGenome gene)
+    {
+        // TODO: Implement procedural mesh generation based on gene parameters
+        return new ArrayMesh();
+    }
+}
