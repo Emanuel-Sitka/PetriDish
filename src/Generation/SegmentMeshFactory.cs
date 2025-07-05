21c80cd9eed5cf32564e3ec7dc5f6358455c32e2
Marius Wespe <mariuswespe@gmail.com>
2025-07-05 13:48:51 +0200

    feat: First iteration of vertex ring z position calculation

--- src/Generation/SegmentMeshFactory.cs
+++ src/Generation/SegmentMeshFactory.cs
@@ -7,7 +7,38 @@ public static class SegmentMeshFactory
 {
     private static int _verticesPerRing = 16;
     private static int _rings_per_segment = 9;
+
+
+    private static float[] CalculateRingZPositions(SegmentGenome genome, float bulgeRadius)
+    {
         
+        var startToBulgeDistance = genome.Length * float.Sqrt(bulgeRadius *  bulgeRadius - genome.StartRadius * genome.StartRadius)
+                                   / (float.Sqrt(bulgeRadius *  bulgeRadius - genome.StartRadius * genome.StartRadius) 
+                                      +  float.Sqrt(bulgeRadius *  bulgeRadius - genome.EndRadius * genome.EndRadius)); 
+        
+        var endToBulgeDistance = genome.Length - startToBulgeDistance;
+        
+        var bulgeZPosition = genome.Length / 2 - startToBulgeDistance;
+        
+        var zPositionPerRing = new float[_rings_per_segment];
+        var inBetweenRingsPerSegmentHalf = (_rings_per_segment - 3) / 2;
+        
+        zPositionPerRing[0] = genome.Length / 2;
+        zPositionPerRing[_rings_per_segment -1] = - genome.Length / 2;
+        zPositionPerRing[inBetweenRingsPerSegmentHalf + 1 ] = bulgeZPosition;
+        
+        var startToBulgeStep = (genome.Length / 2  -  bulgeZPosition) / (endToBulgeDistance - startToBulgeDistance +1 );
+        var endToBulgeStep = (bulgeZPosition -  (genome.Length / 2) ) / (endToBulgeDistance - startToBulgeDistance +1 );
+        for (var i = 1; i < inBetweenRingsPerSegmentHalf + 1; i++)
+        {
+            zPositionPerRing[i] = zPositionPerRing[0] - startToBulgeStep * i;
+            zPositionPerRing[_rings_per_segment - 1 - i] = zPositionPerRing[_rings_per_segment - 1] + endToBulgeStep * i
+        }
+
+        return zPositionPerRing;
+    }
+    
+    
     public static ArrayMesh CreateMesh(SegmentGenome genome)
     {
         
@@ -16,16 +47,15 @@ public static class SegmentMeshFactory
         // Arithmetic preparations
         var bulgeRadius = genome.MidBulge * float.Max(genome.StartRadius, genome.EndRadius);
         
-        var startToBulgeDistance = genome.Length * float.Sqrt(bulgeRadius *  bulgeRadius - genome.StartRadius * genome.StartRadius)
-            / (float.Sqrt(bulgeRadius *  bulgeRadius - genome.StartRadius * genome.StartRadius) 
-               +  float.Sqrt(bulgeRadius *  bulgeRadius - genome.EndRadius * genome.EndRadius)); 
         
-        var endToBulgeDistance = genome.Length - startToBulgeDistance;
+        var zPositionsPerRinge = CalculateRingZPositions(genome, bulgeRadius);
         
-        var bulge_z_position = genome.Length / 2 - startToBulgeDistance;
+        var xRadiusPerRing = new float[_rings_per_segment];
+        var yRadiusPerRing = new float[_rings_per_segment];
+
         
-        // Calculating the z_positions of the vertex rings
-        var in_between_rings_per_segment_half = (_rings_per_segment - 3) / 2;
+
+        
         
         
         // TODO: Implement procedural mesh generation based on gene parameters
