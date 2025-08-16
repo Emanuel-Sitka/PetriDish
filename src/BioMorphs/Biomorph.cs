f995ab37843a051485ce08f47e1650e1a3a9bab9
Emanuel-Sitka <emanuel.sitka@outlook.com>
2025-08-16 13:06:34 +0200

    feat: first iteration of Biomorph class

---
+++ src/BioMorphs/Biomorph.cs
@@ -0,0 +1,47 @@
+using Godot;
+using PetriDish.Genome;
+
+namespace PetriDish.BioMorphs;
+
+public partial class Biomorph : Node3D
+{
+    public BiomorphGenome Genome { get; set; }
+    
+    [Export]
+    public PackedScene SegmentScene { get; set; }
+
+    public override void _Ready()
+    {
+        Genome = new BiomorphGenome()
+        {
+            SpineSegmentGenomes = new List<SegmentGenome>()
+            {
+                new SegmentGenome(4f, 2f, 2f, 1.5f),
+                new SegmentGenome(6f, 1.5f, 1.5f, 1.3f),
+            }
+        };
+
+        var totalLength = Genome.TotalLength;
+
+        var frontEndOfRemainingSpine = totalLength / 2;
+        
+        
+
+        foreach (var segmentGenome in Genome.SpineSegmentGenomes)
+        {
+            var segment =  new Segment()
+                {Length = segmentGenome.Length, 
+                    StartRadius = segmentGenome.StartRadius,  
+                    EndRadius = segmentGenome.EndRadius, 
+                    MidBulge = segmentGenome.MidBulge};
+
+            var currentAttachmentPoint =
+                new Transform3D().Translated(new Vector3(0f, 0f, frontEndOfRemainingSpine - segment.Length / 2));
+            
+            segment.Transform = currentAttachmentPoint;
+            AddChild(segment);
+            
+            frontEndOfRemainingSpine -= segment.Length;
+        }
+    }
+}
--- src/Genome/BiomorphGenome.cs
+++ src/Genome/BiomorphGenome.cs
@@ -3,4 +3,6 @@ namespace PetriDish.Genome;
 public class BiomorphGenome
 {
     public List<SegmentGenome> SpineSegmentGenomes = new List<SegmentGenome>();
+    
+    public float TotalLength => SpineSegmentGenomes.Sum(segment => segment.Length);
 }
